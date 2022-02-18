e8b759793c0a3e794c09180519bf40b0343065f7
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-18 16:00:16 -0500

    getting resources successfully

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,5 +1,9 @@
-function getResources() {
-    return Promise.resolve(`success! here are the resources`)
+const db = require('../../data/dbConfig')
+
+async function getResources() {
+    const resources = await db('resources')
+    return resources
+    // return Promise.resolve(`success! here are the resources`)
  }
  
  module.exports = {getResources}
